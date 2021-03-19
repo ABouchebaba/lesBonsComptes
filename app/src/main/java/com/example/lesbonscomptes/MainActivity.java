91c4d287ed3a4a732805dc2d1fd1a7c36e57c4bb
Amine Bouchebaba <ABouchebaba@outlook.com>
2021-03-19 12:16:43 +0100

    added initDB in mainActivity

--- app/src/main/java/com/example/lesbonscomptes/MainActivity.java
+++ app/src/main/java/com/example/lesbonscomptes/MainActivity.java
@@ -16,8 +16,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lesbonscomptes.db.DbHelper;
+import com.example.lesbonscomptes.models.Expenditure;
 import com.example.lesbonscomptes.models.Group;
+import com.example.lesbonscomptes.models.Member;
+import com.example.lesbonscomptes.models.Participant;
 
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db = new DbHelper(this);
+        initDB();// needs to be called after db variable is initialized
+
         mGroupsListLayout = findViewById(R.id.groupsList);
         refreshView();
     }
@@ -162,5 +168,52 @@ public class MainActivity extends AppCompatActivity {
             group.addView(delete);
             mGroupsListLayout.addView(group);
         }
+    }
+
+    private void initDB() {
+        // start from scratch
+
+        // Dropping tables
+        db.getWritableDatabase().execSQL(Participant.dropQuery());
+        db.getWritableDatabase().execSQL(Expenditure.dropQuery());
+        db.getWritableDatabase().execSQL(Member.dropQuery());
+        db.getWritableDatabase().execSQL(Group.dropQuery());
+
+        // Creating tables again
+        db.getWritableDatabase().execSQL(Group.createQuery());
+        db.getWritableDatabase().execSQL(Member.createQuery());
+        db.getWritableDatabase().execSQL(Expenditure.createQuery());
+        db.getWritableDatabase().execSQL(Participant.createQuery());
+
+        // Populate tables
+        Group g1 = new Group(null, "G1");
+        g1 = g1.save(db);
+
+        Group g2 = new Group(null, "G2");
+        g2 = g2.save(db);
+        ////////////////////////////////////////////////////
+
+        Member m1 = new Member(null, "m1", "099999999", g1.getId());
+        m1 = m1.save(db);
+
+        Member m2 = new Member(null, "m2", "088888888", g1.getId());
+        m2 = m2.save(db);
+
+        Member m3 = new Member(null, "m3", "077777777", g1.getId());
+        m3 = m3.save(db);
+        ///////////////////////////////////////////////////
+
+        Expenditure e1 = new Expenditure(null, 900.f, new Date(), "Tacos", m1.getId(), g1.getId());
+        e1 = e1.save(db);
+
+        Expenditure e2 = new Expenditure(null, 600.f, new Date(), "Pizza", m2.getId(), g1.getId());
+        e2 = e2.save(db);
+        ///////////////////////////////////////////////////
+
+        Participant p1 = new Participant(null, e1.getId(), m2.getId());
+        p1 = p1.save(db);
+
+        Participant p2 = new Participant(null, e1.getId(), m3.getId());
+        p2 = p2.save(db);
     }
 }
